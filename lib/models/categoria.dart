fdf277241c98dcf4f6dbe044e1efb0151fb1517a
SantiagoVanegas <luis.vanegas01@uceva.edu.co>
2024-05-27 21:00:19 -0500

    Se muestra el correo

--- lib/models/categoria.dart
+++ lib/models/categoria.dart
@@ -1,14 +1,14 @@
 class Categoria {
   final int id_categoria;
   final String nombre_categoria;
-  
 
-  Categoria(
-      {required this.id_categoria, required this.nombre_categoria});
+  Categoria({required this.id_categoria, required this.nombre_categoria});
 
   factory Categoria.fromJson(Map<String, dynamic> json) {
     return Categoria(
-      id_categoria: json['id_categoria'] is int ? json['id_categoria'] : int.parse(json['id_categoria'].toString()),
+      id_categoria: json['id_categoria'] is int
+          ? json['id_categoria']
+          : int.parse(json['id_categoria'].toString()),
       nombre_categoria: json['nombre_categoria'],
     );
   }
@@ -17,7 +17,6 @@ class Categoria {
     return {
       'id_categoria': id_categoria,
       'nombre_categoria': nombre_categoria,
-
     };
   }
 }
--- lib/models/correo.dart
+++ lib/models/correo.dart
@@ -1,1 +1,5 @@
+class Correo {
+  final String correo;
 
+  Correo({required this.correo});
+}
