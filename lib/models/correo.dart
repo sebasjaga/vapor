dd58a4be21c52fcf5253d3d953d0cc90d36d48d5
JarothMC <jaroth.mora01@uceva.edu.co>
2024-05-27 21:07:02 -0500

    se hace llamado para retornar el correo

--- lib/models/correo.dart
+++ lib/models/correo.dart
@@ -2,4 +2,8 @@ class Correo {
   final String correo;
 
   Correo({required this.correo});
+
+  factory Correo.fromJson(Map<String, dynamic> json) {
+    return Correo(correo: json['correo ']);
+  }
 }
